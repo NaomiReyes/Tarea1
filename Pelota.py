ff6cb31e157ca216d7f3710fe780a93ecb40a3d3
NaomiReyes <nreyes@ciencias.unam.mx>
2019-08-13 23:40:07 -0500

    Listo

--- Pelota.py
+++ Pelota.py
@@ -1,3 +1,10 @@
-#Clase que modela pelotas.
+class Pelotas(Fabrica):
 
-class Pelota:
+	def _init_(self, pre_prod, ganan, tamano, color):
+		Fabrica._init_(self, "Pelota", pre_prod, ganan)
+		self.tam = tamano
+		self.col = color
+	def pelota (self):
+		print "Tu pelota es " + self.tamano + "de color " + self.color
+ 
+
