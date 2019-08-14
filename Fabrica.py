4783a463bf1035c30f0027419b56271bbad5dfd4
NaomiReyes <nreyes@ciencias.unam.mx>
2019-08-13 22:42:21 -0500

    Fabrica Lista

--- Fabrica.py
+++ Fabrica.py
@@ -1,6 +1,22 @@
 #Clase que modela una fabrica de juguetes.
 
 class Fabrica:
+	def _init_(self, nom, pre_prod, ganan):
+		self.nombre = nom
+		self.costoPro = pre_prod
+		self.ganancia = gana
+		self.descuento = False
+	def costo:
+		if self.descuento:
+			des = int(input("¿Cuanto es el descuento?"))
+			costofin = (self.costoPro + self.ganancia) - des
+			print costofin
+		else:
+			print self.costoPro + self.ganancia
+	def nombreProducto:
+		print self.nombre
+		
+			
 
 
 
