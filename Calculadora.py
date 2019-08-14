ad5cad0fd3963860078e4c1b5864fb302c83e76d
NaomiReyes <nreyes@ciencias.unam.mx>
2019-08-13 22:19:02 -0500

    Nuevas creaciones

--- Calculadora.py
+++
@@ -1,4 +0,0 @@
-#Esto sera un programa que haga operacione de de dos numeros sin importar ue sean complejos o reales.
-
-
-
---
+++ Fabrica.py
@@ -0,0 +1,6 @@
+#Clase que modela una fabrica de juguetes.
+
+class Fabrica:
+
+
+
